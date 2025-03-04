b8bd35cf3022461f31004c026a662c802115a341
EoghanOConnor <eoconnor@redhat.com>
2025-03-04 14:26:14 +0000

    init

--- experiments/max_params_per_tool/max_params_per_tool.py
+++ experiments/max_params_per_tool/max_params_per_tool.py
@@ -9,35 +9,40 @@ from ..tools import ArbitraryClientTool
 
 client = LlamaStackClient(base_url="http://localhost:8321")
 
-for i in range(15,30,5):
-    print(i)
-    
-    arbitrary_client_tool = ArbitraryClientTool(i)
-    print(arbitrary_client_tool.get_tool_definition())
+i = 5
+print(i)
 
-    agent_config = AgentConfig(
-        model="meta-llama/Llama-3.1-8B-Instruct",
-        enable_session_persistence = False,
-        instructions = "You are a helpful assistant.",
-        toolgroups = [],
-        client_tools = [arbitrary_client_tool.get_tool_definition()],
-        tool_choice="required",
-        tool_prompt_format="json",
-        max_infer_iters=4,
-        )
+arbitrary_client_tool = ArbitraryClientTool(i)
+print(arbitrary_client_tool.get_tool_definition())
 
-    agent = Agent(client=client,
-                agent_config=agent_config,
-                client_tools=[arbitrary_client_tool]
-                )
+agent_config = AgentConfig(
+    model="meta-llama/Llama-3.2-3B-Instruct",
+    enable_session_persistence = False,
+    instructions = "You are a helpful assistant.",
+    toolgroups = [],
+    client_tools = [arbitrary_client_tool.get_tool_definition()],
+    tool_choice="required",
+    tool_prompt_format="python_list",
+    max_infer_iters=4,
+    )
 
-    session_id = agent.create_session("test")
-    response = agent.create_turn(
-                messages=[{"role":"user","content":"You must use the arbitrary_client_tool"}],
-                session_id= session_id,
-                )
+agent = Agent(client=client,
+            agent_config=agent_config,
+            client_tools=[arbitrary_client_tool]
+            )
 
-    for r in EventLogger().log(response):
-        r.print()
-    
-    print("\n")
+session_id = agent.create_session("test")
+response = agent.create_turn(
+            messages=[{"role":"user","content":"You must use the arbitrary_client_tool"}],
+            session_id= session_id,
+            )
+
+for r in EventLogger().log(response):
+    r.print()
+
+print("\n")
+
+result = arbitrary_client_tool.run_impl( query_1="test2", query_2="test3", query_3="test4", query_4="test5")
+
+# The result will be the same parameters as the input:
+print(result)
