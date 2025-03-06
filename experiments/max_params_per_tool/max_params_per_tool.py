5912eae48a715ee98dd294d2a0f1c14fec0294a3
EoghanOConnor <eoconnor@redhat.com>
2025-03-06 17:57:08 +0000

    WIP

--- experiments/max_params_per_tool/max_params_per_tool.py
+++ experiments/max_params_per_tool/max_params_per_tool.py
@@ -2,47 +2,80 @@ from llama_stack_client.lib.agents.event_logger import EventLogger
 from llama_stack_client.lib.agents.agent import Agent
 from llama_stack_client.types.agent_create_params import AgentConfig
 from llama_stack_client import LlamaStackClient
-
-from ..tools import ArbitraryClientTool
+import json
+from ..tools import ArbitraryClientTool, GenerateParam
 
 
 
 client = LlamaStackClient(base_url="http://localhost:8321")
 
-i = 5
-print(i)
 
-arbitrary_client_tool = ArbitraryClientTool(i)
-print(arbitrary_client_tool.get_tool_definition())
-
-agent_config = AgentConfig(
-    model="meta-llama/Llama-3.2-3B-Instruct",
-    enable_session_persistence = False,
-    instructions = "You are a helpful assistant.",
-    toolgroups = [],
-    client_tools = [arbitrary_client_tool.get_tool_definition()],
-    tool_choice="required",
-    tool_prompt_format="python_list",
-    max_infer_iters=4,
-    )
-
-agent = Agent(client=client,
-            agent_config=agent_config,
-            client_tools=[arbitrary_client_tool]
+    
+generate_param_tool = GenerateParam("param","str","This is a parameter")
+# print(generate_param_tool.get_tool_definition())
+agent_config_param = AgentConfig(
+model="meta-llama/Llama-3.1-8B-Instruct",
+enable_session_persistence = False,
+instructions = """You are a Parameter generator assistant. Use the GenerateParam tool to generate random realistic parameters for a function that will be created.
+When using the GenerateParam tool:
+1. Create a name, parameter type, and description for the parameters
+2. Return the name, parameter type, and description for the parameters
+3. Present the result clearly""",
+toolgroups = [],
+client_tools = [generate_param_tool.get_tool_definition()],
+tool_choice="auto",
+tool_prompt_format="json",
+max_infer_iters=4,
+)
+agent_param = Agent(client=client,
+            agent_config=agent_config_param,
+            client_tools=[generate_param_tool],
             )
 
-session_id = agent.create_session("test")
-response = agent.create_turn(
-            messages=[{"role":"user","content":"You must use the arbitrary_client_tool"}],
+session_id = agent_param.create_session("test")
+response = agent_param.create_turn(
+            messages=[{"role":"user","content":"use the GenerateParam and pass in a random parameters"}],
             session_id= session_id,
             )
 
+print("OUTPUT:")
+# for r in response:
+    # print("Inference Output:")
+    # print(r)
+print("\n \n")
+
+# arbitrary_client_tool = ArbitraryClientTool(i)
+# print(arbitrary_client_tool.get_tool_definition())
+# agent_config = AgentConfig(
+#     model="meta-llama/Llama-3.1-8B-Instruct",
+#     enable_session_persistence = False,
+#     instructions = "You are a helpful assistant.",
+#     toolgroups = [],
+#     client_tools = [arbitrary_client_tool.get_tool_definition()],
+#     tool_choice="auto",
+#     tool_prompt_format="json",
+#     max_infer_iters=4,
+#     )
+
+# agent = Agent(client=client,
+#             agent_config=agent_config,
+#             client_tools=[arbitrary_client_tool]
+#             )
+
+    # session_id = agent.create_session("test")
+    # response = agent.create_turn(
+    #             messages=[{"role":"user","content":"use the arbitrary_client_tool and pass in parameters"}],
+    #             session_id= session_id,
+    #             )
+print("OUTPUT:")
+ev = ""
 for r in EventLogger().log(response):
-    r.print()
+    ev += str(r) 
+print(ev)
 
+data = json.loads(ev)
+print(data)
+# print(abbbs)
+# print(response.ToolCall())
+print("END:") 
 print("\n")
-
-result = arbitrary_client_tool.run_impl( query_1="test2", query_2="test3", query_3="test4", query_4="test5")
-
-# The result will be the same parameters as the input:
-print(result)
--- experiments/tools.py
+++ experiments/tools.py
@@ -10,23 +10,58 @@ class ArbitraryClientTool(ClientTool):
         return kwargs
 
     def _generate_kwargs(self, num:int) -> dict:
-       kwargs = {f"query_{n}": Parameter(
-                name=f"query_{n}",
+       kwargs = {f"q_{n}": Parameter(
+                name=f"q_{n}",
                 parameter_type="str",
-                description=f"query_{n}",
+                description=f"q_{n}",
                 required=True,
             ) for n in range(num)}
        
        return kwargs
 
     def get_name(self):
-        return "arbitrary_tool"
+        return "arbitrary_client_tool"
     
     def get_description(self):
         return "This tool is used to evaluate the number of parameters an LLM can manage during tool calling."
     
     def get_params_definition(self):
         return self._generate_kwargs(self.n)
+    
+    def run_impl(self, **kwargs):
+        return self._arbitrary_tool(kwargs)
+
+
+
+# Tool params
+# Should
+class GenerateParam(ClientTool):
+    def __init__(self, name, parameter_type, description):
+        self.name = name
+        self.parameter_type = parameter_type
+        self.description = description
+    
+    def _arbitrary_tool(self, *kwargs):
+        return kwargs
+
+    def _generate_kwargs(self, name:str,parameter_type:str,description:str) -> dict:
+       kwargs = {f"Param": Parameter(
+                name=f"{name}",
+                parameter_type=f"{parameter_type}",
+                description=f"{description}",
+                required=True,
+            )}
+       
+       return kwargs
+
+    def get_name(self):
+        return "generate_param_tool"
+    
+    def get_description(self):
+        return "This tool generates a random realistic parameter, with a name, type, and description."
+    
+    def get_params_definition(self):
+        return self._generate_kwargs(self.name,self.parameter_type,self.description)
     
     def run_impl(self, **kwargs):
         return self._arbitrary_tool(kwargs)
