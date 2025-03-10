5e213e4628b017a468bf53fe05ec8ea4e0037072
EoghanOConnor <eoconnor@redhat.com>
2025-03-10 16:29:47 +0000

    Multiple generated params

--- experiments/max_params_per_tool/max_params_per_tool.py
+++ experiments/max_params_per_tool/max_params_per_tool.py
@@ -8,18 +8,18 @@ from ..tools import ArbitraryClientTool, GenerateParam
 client = LlamaStackClient(base_url="http://localhost:8321")
 
 # GENERATE PARAM TOOL
-i=2
+i=20
 
 generate_param_tool = GenerateParam("param","str","This is a parameter")
-# print(generate_param_tool.get_tool_definition())
 agent_config_param = AgentConfig(
 model="meta-llama/Llama-3.1-8B-Instruct",
 enable_session_persistence = False,
 instructions = """You are a Parameter generator assistant. Use the GenerateParam tool to generate realistic and random parameters for a function that will be created.
 When using the generate_param_tool tool:
-1. Create one realistic name, select a parameter type to match it, and a description.
-2. Pass these into the generate_param_tool
-3. Present the result clearly""",
+1. Choose a random topic and parameter type (str/bool/int/float).
+3. For the randomly selected parameter type and topic create a realistic name to match the parameter type, and a description also.
+4. Pass these the name, parameter type and description in to the generate_param_tool
+""",
 toolgroups = [],
 client_tools = [generate_param_tool.get_tool_definition()],
 tool_choice="auto",
@@ -30,44 +30,33 @@ agent_param = Agent(client,agent_config_param,
             tools=[generate_param_tool],
             )
 all_params = {"name": [], "type": [], "description": []}
-# for count in i:
+
 session_id = agent_param.create_session("test")
-response = agent_param.create_turn(
-            messages=[{"role":"user","content":"use the generate_param_tool and pass in a name, type, and description"}],
-            session_id= session_id,
-            stream=False,
-            )
+for count in range(i):
+    response = agent_param.create_turn(
+                messages=[{"role":"user","content":"use the generate_param_tool and pass in a name, type, and description"}],
+                session_id= session_id,
+                stream=False,
+                )
 
-steps = response.steps
-#######################################
-#############
-# Set Steam = False to use the steps
-for step in steps:
-    print(step)
-    print("\n")
-# # Set Stream = True to use the EventLogger
-# for log in EventLogger().log(response):
-#         log.print()
-# assert len(steps) == 3
+    steps = response.steps
 
-param = steps[0].api_model_response.tool_calls[0].arguments['param']
+    param = steps[0].api_model_response.tool_calls[0].arguments['param']
 
-name = param.get('name', 'Not Available')
-type = param.get('type', 'Not Available')
-description = param.get('description', 'Not Available')
-            
+    name = param.get('name', 'Not Available')
+    type = param.get('type', 'Not Available')
+    description = param.get('description', 'Not Available')
+                
 
-print(f" name {name}, type:{type}, description:{description}")
-all_params["name"].append(name)
-all_params["type"].append(type)
-all_params["description"].append(description)
+    print(f" name {name}, type:{type}, description:{description}")
+    all_params["name"].append(name)
+    all_params["type"].append(type)
+    all_params["description"].append(description)
 
 # ARBITRARY CLIENT TOOL
-# all_params = {"name": ["speed"], "type": ["int"], "description": ["Car speed in km/hr"]}
-
 i=1
 arbitrary_client_tool = ArbitraryClientTool(all_params)
-print(arbitrary_client_tool.get_tool_definition())
+
 agent_config = AgentConfig(
     model="meta-llama/Llama-3.1-8B-Instruct",
     enable_session_persistence = False,
@@ -89,10 +78,6 @@ response = agent.create_turn(
             messages=[{"role":"user","content":"use the arbitrary_client_tool and pass in parameters"}],
             session_id= session_id
             )
-# steps = response.steps
-# for step in steps:
-#     print(step)
-#     print("\n")
 
 for log in EventLogger().log(response):
         log.print()
